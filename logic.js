620225ef56c93c99c712c8ca31170cbd9ca2578f
shaikfazal-del <shaikfazal9392@gmail.com>
2025-08-13 13:38:29 +0530

    Initial commit - Product MVP basic structure

---
+++ logic.js
@@ -0,0 +1,7 @@
+document.getElementById("learnMoreBtn").addEventListener("click", () => {
+    alert("FastLaunch helps you build and deploy MVPs in days, not months!");
+});
+
+document.getElementById("signUpBtn").addEventListener("click", () => {
+    window.location.href = "https://example.com/signup";
+});
